77c1555bb72a2cd5bd683869b3a08ed044b7c0c5
Роман Лобач <roma@MacBook-Air-Roman.local>
2025-11-05 09:43:53 +0200

    Add structs for input validation and configuration

--- DataInputService.h
+++ DataInputService.h
@@ -3,29 +3,44 @@
 
 #include <string>
 
+struct ValidationResult {
+    bool valid;
+    std::string message;
+};
+
+struct Config {
+    size_t kMinLenOfName = 1;
+    size_t kMaxLenOfName = 64;
+
+    size_t kMinPasswordLength = 8;
+    size_t kMaxPasswordLength = 64;
+
+    size_t kMinTagLength = 2;
+    size_t kMaxTagLength = 32;
+
+    size_t kMinEmailLocalPartLength = 1;
+    size_t kMaxEmailLocalPartLength = 64;
+    std::string kEmailDomain = "@gmail.com";
+};
+
+struct UserInput {
+    std::string name;
+    std::string email;
+    std::string password;
+    std::string tag;
+};
+
 namespace DataInputService {
-    namespace detail {
-        inline constexpr int kMinPasswordLength = 8;
-        inline constexpr int kMaxPasswordLength = 22;
-        inline constexpr int kMinTagLength = 4;
-        inline constexpr int kMaxTagLength = 11;
-        inline constexpr int kMinLenOfName = 4;
-        inline constexpr int kMaxLenOfName = 20;
-        inline constexpr int kMinEmailLocalPartLength = 4;
-        inline constexpr int kMaxEmailLocalPartLength = 26;
-        inline const std::string kEmailDomain = "@gmail.com";
-    }
+    ValidationResult emailValid(const std::string& login);
 
-    bool emailValid(const std::string& login);
+    ValidationResult passwordValid(const std::string& password);
+    ValidationResult passwordValidLength(const std::string& password);
+    ValidationResult passwordValidCharacters(const std::string& password);
 
-    bool passwordValid(const std::string& password);
-    bool passwordValidLength(const std::string& password);
-    bool passwordValidCharacters(const std::string& password);
+    ValidationResult tagValid(const std::string& tag);
+    ValidationResult tagValidCharacters(const std::string& tag);
 
-    bool tagValid(const std::string& tag);
-    bool tagValidCharacters(const std::string& tag);
-
-    bool nameValid(const std::string& name);
+    ValidationResult nameValid(const std::string& name);
 
 } // namespace DataInputService
 
